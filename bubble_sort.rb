37f6cd663fb81081ead0c80bd1f387afe6233348
jcanz <3466642+jcanzonetta@users.noreply.github.com>
2020-08-17 18:58:20 -0700

    Add bubble_sort.rb

---
+++ bubble_sort.rb
@@ -0,0 +1,18 @@
+def bubble_sort(inputArray)
+    len = inputArray.length
+
+    sorts = 0
+
+    for i in 0..(len - 2)
+        if inputArray[i] > inputArray[i+1]
+            temp = inputArray[i+1]
+            inputArray[i+1] = inputArray[i]
+            inputArray[i] = temp
+            sorts += 1
+        end
+    end
+
+    sorts == 0 ? (return inputArray) : (return bubble_sort(inputArray.slice(0, len - 1)) << inputArray[len-1])
+end
+
+p bubble_sort([4,3,78,2,0,2])
