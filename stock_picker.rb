bd42c5ff52e90a94079696cb0feb1ae3a238a4db
jcanz <3466642+jcanzonetta@users.noreply.github.com>
2020-08-20 22:12:28 -0700

    Rubo cop linting/formatting

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,25 +1,24 @@
-def stock_picker (price_history)
-    transaction = Hash.new(0)
+def stock_picker(price_history)
+  transaction = Hash.new(0)
 
-    price_history.each_with_index { |current_price, current_day|
-        remaining_array = price_history.slice(current_day, price_history.length)
-        sell_price = remaining_array.max
-        profit = sell_price - current_price
+  price_history.each_with_index do |current_price, current_day|
+    remaining_array = price_history.slice(current_day, price_history.length)
+    sell_price = remaining_array.max
+    profit = sell_price - current_price
 
-        if profit > transaction[:best_profit]
-            transaction = {
-                buy_price: current_price,
-                buy_day: current_day,
-                sell_price: sell_price,
-                sell_day: remaining_array.index(sell_price) + current_day,
-                best_profit: sell_price - current_price
-            }
-        end
+    next unless profit > transaction[:best_profit]
+
+    transaction = {
+      buy_price: current_price,
+      buy_day: current_day,
+      sell_price: sell_price,
+      sell_day: remaining_array.index(sell_price) + current_day,
+      best_profit: sell_price - current_price
     }
-    p "Buy the stock on day #{transaction[:buy_day]} for $#{transaction[:buy_price]},"
-    p "and sell the stock on day #{transaction[:sell_day]} for $#{transaction[:sell_price]},"
-    p "for the highest profit of $#{transaction[:best_profit]}."
+  end
+  p "Buy the stock on day #{transaction[:buy_day]} for $#{transaction[:buy_price]},"
+  p "and sell the stock on day #{transaction[:sell_day]} for $#{transaction[:sell_price]},"
+  p "for the highest profit of $#{transaction[:best_profit]}."
 end
 
-
-stock_picker([17,3,6,9,15,8,6,1,10])
+stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
--- substrings.rb
+++ substrings.rb
@@ -1,41 +1,41 @@
-def substrings (words, dictionary)
-    result = Hash.new(0)
-    
-    str_arr = convert_string(words)
+def substrings(words, dictionary)
+  result = Hash.new(0)
 
-    str_arr.each do |str|
-            recursive_slice(str.downcase, dictionary, result)
-            single_slice(str, dictionary, result)
-    end
+  str_arr = convert_string(words)
 
-    return p result # Hash listing each substring that was found, and how many times it exists in the dictionary.
+  str_arr.each do |str|
+    recursive_slice(str.downcase, dictionary, result)
+    single_slice(str, dictionary, result)
+  end
+
+  p result # Hash listing each substring that was found, and how many times it exists in the dictionary.
 end
 
 # checks each individual character in a string and checks if it's in the dictionary
 def single_slice(str, dictionary, result)
-    str.each_char do |letter|
-        increment_hash(letter, dictionary, result)
-    end
+  str.each_char do |letter|
+    increment_hash(letter, dictionary, result)
+  end
 end
 
 # recursively runs the increment_hash on every substring combination except for single letters
 def recursive_slice(substring, dictionary, result)
-    if substring.length > 1
-        increment_hash(substring, dictionary, result)
-        recursive_slice(substring.slice(0, substring.length - 1), dictionary, result)
-        recursive_slice(substring.slice(1, substring.length), dictionary, result)
-    end
+  if substring.length > 1
+    increment_hash(substring, dictionary, result)
+    recursive_slice(substring.slice(0, substring.length - 1), dictionary, result)
+    recursive_slice(substring.slice(1, substring.length), dictionary, result)
+  end
 end
 
 # converts the string  to an array separated by the parameters in the Regex
-def convert_string (string)
-    return string.split(/[,!'? ]/)
+def convert_string(string)
+  string.split(/[,!'? ]/)
 end
 
 # increments the value of the key if the word is in the dictionary
-def increment_hash (word, dictionary, result)
-    return dictionary.index(word) != nil ? result[word] += 1 : nil
+def increment_hash(word, dictionary, result)
+  !dictionary.index(word).nil? ? result[word] += 1 : nil
 end
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 substrings("Howdy partner, sit down! How's it going?", dictionary)
